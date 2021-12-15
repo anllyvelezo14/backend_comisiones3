ace123d95f379644fa13300a3860089180479861
alwos19 <jhon.jaramilloe@udea.edu.co>
2021-12-14 20:39:51 -0500

    practica2

--- app/routes.js
+++ app/routes.js
@@ -1,19 +1,15 @@
-const expres = require('express');
-const router = expres.Router();
+const expres = require('express')
+const router = expres.Router()
 
 //Middlerwares
-<<<<<<< HEAD
+
 // const auth = require('./middlerwares/auth');
 // const upload = require('./middlerwares/upload');
-=======
-<<<<<<< HEAD
-const auth = require('./middlerwares/auth');
-const upload = require('./middlerwares/upload');
-=======
-// const auth = require('./middlerwares/auth');
 
->>>>>>> d1410144e3310d9b67fd23d9d6a86556bce65f54
->>>>>>> d293bf755d1ea5dcfe250d2cbf30c1736e90ef2e
+const auth = require('./middlerwares/auth')
+const upload = require('./middlerwares/upload')
+
+// const auth = require('./middlerwares/auth')
 
 //POLICIES
 // const ComisionPolicy = require('./policies/ComisionPolicy');
@@ -27,7 +23,7 @@ const upload = require('./middlerwares/upload');
 
 //CONTROLLERS
 // const AuthController = require('../app/controllers/AuthController');
-const ComisionController = require('./controllers/ComisionController');
+const ComisionController = require('./controllers/ComisionController')
 // const DocumentoController = require('./controllers/DocumentoController');
 // const CumplidoController = require('./controllers/CumplidoController');
 // const EstadoController = require('./controllers/EstadoController');
@@ -39,17 +35,18 @@ const ComisionController = require('./controllers/ComisionController');
 // const ComisionHasEstadoController = require('./controllers/ComisionHasEstadoController');
 // const FileController = require('./controllers/FileController');
 
-
 //HOME
-router.get('/', (req, res) => res.json({ hola: "mundo" }));
-
+router.get('/', (req, res) => res.json({ hola: 'mundo' }))
 
 //GET
-<<<<<<< HEAD
-router.get('/api/comisiones', 
-// auth, 
-// ComisionPolicy.showAll, 
-ComisionController.all);
+
+router.get('/api/comisiones',
+  // auth,
+  // ComisionPolicy.showAll,
+  // ComisionPolicy.showAll,
+  // ComisionPolicy.showAll,
+  ComisionController.all,
+)
 // router.get('/api/documentos', auth, ShowAllDocsCumpl.showAll, DocumentoController.all);
 // router.get('/api/cumplidos', auth, ShowAllDocsCumpl.showAll, CumplidoController.all);
 // router.get('/api/estados', auth, EstadoController.all);
@@ -61,11 +58,17 @@ ComisionController.all);
 // router.get('/api/departamentos', auth, DepartamentoController.all);
 
 //GET BY ID
-router.get('/api/comisiones/:id', 
-// auth, 
-ComisionController.find, 
-// ComisionPolicy.show, 
-ComisionController.show);
+router.get(
+  '/api/comisiones/:id',
+  // auth,
+  ComisionController.find,
+  ComisionController.find,
+  ComisionController.find,
+  // ComisionPolicy.show,
+  // ComisionPolicy.show,
+  // ComisionPolicy.show,
+  ComisionController.show,
+)
 // router.get('/api/cumplidos/:id', auth, CumplidoController.find, CumplidoPolicy.show, CumplidoController.show);
 // router.get('/api/tipos-solicitud/:id', auth, TipoSolicitudController.find, TipoSolicitudController.show);
 // router.get('/api/estados/:id', auth, EstadoController.find, EstadoController.show);
@@ -78,7 +81,6 @@ ComisionController.show);
 //GET NAME
 // router.get('/api/tipos-solicitud/:nombre', auth, TipoSolicitudController.showName);
 
-
 //CREATE
 // router.post('/api/comisiones', auth, ComisionPolicy.create, ComisionController.create, ComisionHasEstadoController.createSolicitada);
 // router.post('/api/cumplidos', auth, CumplidoController.create);
@@ -86,71 +88,11 @@ ComisionController.show);
 // router.post('/api/estados', auth, EstadoPolicy.create, EstadoController.create);
 // router.post('/api/comisiones-estados', auth, ComisionHasEstadoPolicy.create, ComisionHasEstadoController.create);//, ComisionHasEstadoController.findComisionbyId);
 // router.post('/api/usuarios', auth, UsuariosPolicy.create, UsuarioController.create);
-=======
-router.get('/api/comisiones', ComisionController.all);
-router.get('/api/documentos');
-router.get('/api/cumplidos');
-router.get('/api/estados');
-router.get('/api/comisiones-estados');
-router.get('/api/tipos-solicitud');
-router.get('/api/usuarios');
-router.get('/api/facultades');
-router.get('/api/roles');
-router.get('/api/departamentos');
-
-//GET BY ID
-<<<<<<< HEAD
-router.get('/api/comisiones/:id', auth, ComisionController.find, ComisionPolicy.show, ComisionController.show);
-router.get('/api/cumplidos/:id', auth, CumplidoController.find, CumplidoPolicy.show, CumplidoController.show);
-router.get('/api/tipos-solicitud/:id', auth, TipoSolicitudController.find, TipoSolicitudController.show);
-router.get('/api/estados/:id', auth, EstadoController.find, EstadoController.show);
-router.get('/api/comisiones-estados/:id', auth, ComisionHasEstadoController.find, ComisionHasEstadoPolicy.show, ComisionHasEstadoController.show);
-router.get('/api/usuarios/:id', auth, UsuarioController.find, UsuariosPolicy.show, UsuarioController.show);
-router.get('/api/facultades/:id', auth, FacultadController.show);
-router.get('/api/roles/:id', auth, RolController.show)
-router.get('/api/departamentos/:id', auth, DepartamentoController.show);
-=======
-router.get('/api/comisiones/:id');
-router.get('/api/documentos/:id');
-router.get('/api/cumplidos/:id');
-router.get('/api/tipos-solicitud/:id');
-router.get('/api/estados/:id');
-router.get('/api/comisiones-estados/:id');
-router.get('/api/usuarios/:id');
-router.get('/api/facultades/:id');
-router.get('/api/roles/:id')
-router.get('/api/departamentos/:id');
->>>>>>> d1410144e3310d9b67fd23d9d6a86556bce65f54
 
 //GET NAME
-router.get('/api/tipos-solicitud/:nombre');
-
-
-//CREATE
-<<<<<<< HEAD
-router.post('/api/comisiones', auth, ComisionPolicy.create, ComisionController.create, ComisionHasEstadoController.createSolicitada);
-router.post('/api/cumplidos', auth, CumplidoController.create);
-router.post('/api/tipos-solicitud', auth, TipoSolicitudPolicy.create, TipoSolicitudController.create);
-router.post('/api/estados', auth, EstadoPolicy.create, EstadoController.create);
-router.post('/api/comisiones-estados', auth, ComisionHasEstadoPolicy.create, ComisionHasEstadoController.create); //, ComisionHasEstadoController.findComisionbyId);
-router.post('/api/usuarios', auth, UsuariosPolicy.create, UsuarioController.create);
-=======
-router.post('/api/comisiones');
-router.post('/api/documentos');
-router.post('/api/cumplidos');
-router.post('/api/tipos-solicitud');
-router.post('/api/estados');
-router.post('/api/comisiones-estados');//, ComisionHasEstadoController.findComisionbyId);
-router.post('/api/usuarios');
->>>>>>> d1410144e3310d9b67fd23d9d6a86556bce65f54
->>>>>>> d293bf755d1ea5dcfe250d2cbf30c1736e90ef2e
-//router.post('/api/facultades', auth, FacultadController.create);
-//router.post('/api/departamentos', auth, DepartamentoController.create);
-
-
+router.get('/api/tipos-solicitud/:nombre')
 
 //UPDATE
-<<<<<<< HEAD
 // router.patch('/api/comisiones/:id', auth, ComisionController.find, ComisionController.estadoComision, ComisionPolicy.update, ComisionController.update);
 // router.patch('/api/documentos/:id', auth, DocumentoController.find, DocumentoController.estadoComision, DocumentoPolicy.update, DocumentoController.update);
 // router.patch('/api/cumplidos/:id', auth, CumplidoController.find, CumplidoController.estadoComision, CumplidoPolicy.update, CumplidoController.update);
@@ -161,7 +103,6 @@ router.post('/api/usuarios');
 // //router.patch('/api/facultades/:id', auth, FacultadController.update);
 // //router.patch('/api/departamentos/:id', auth, DepartamentoController.update);
 
-
 // //DELETE
 // router.delete('/api/comisiones/:id', auth, ComisionController.find, ComisionController.estadoComision, ComisionPolicy.delete, ComisionController.delete);
 // router.delete('/api/documentos/:id', auth, DocumentoController.find, DocumentoController.estadoComision, DocumentoPolicy.delete, DocumentoController.delete);
@@ -171,7 +112,6 @@ router.post('/api/usuarios');
 // router.delete('/api/comisiones-estados/:id', auth, ComisionHasEstadoController.find, ComisionHasEstadoPolicy.delete, ComisionHasEstadoController.delete);
 // //router.delete('/api/facultades/:id', auth, FacultadController.delete);
 // //router.delete('/api/departamentos/:id', auth, DepartamentoController.delete);
-
 
 // //Desactive and active users
 // router.patch('/api/usuarios/desactive/:id', auth, UsuarioController.desactive);
@@ -184,59 +124,10 @@ router.post('/api/usuarios');
 // //router.get("/files",auth, FileController.getListFiles);
 // // router.get("/files/:name",auth, FileController.download);
 
-
 // //Registro y login
 // router.post('/api/signin', AuthController.signIn);
 // router.post('/api/signup', AuthController.signUp);
 // //router.post('/api/recuperar-contrasena', AuthController.changePassword);
 // router.patch('/api/recuperar-contrasena', AuthController.forgotPassword)
-=======
-router.patch('/api/comisiones/:id');
-router.patch('/api/documentos/:id');
-router.patch('/api/cumplidos/:id');
-router.patch('/api/tipos-solicitud/:id');
-router.patch('/api/estados/:id');
-//router.patch('/api/comisiones-estados/:id', auth, ComisionHasEstadoController.update);
-router.patch('/api/usuarios/:id');
-//router.patch('/api/facultades/:id', auth, FacultadController.update);
-//router.patch('/api/departamentos/:id', auth, DepartamentoController.update);
 
-
-//DELETE
-router.delete('/api/comisiones/:id');
-router.delete('/api/documentos/:id');
-router.delete('/api/cumplidos/:id');
-router.delete('/api/tipos-solicitud/:id');
-router.delete('/api/estados/:id');
-router.delete('/api/comisiones-estados/:id');
-//router.delete('/api/facultades/:id', auth, FacultadController.delete);
-//router.delete('/api/departamentos/:id', auth, DepartamentoController.delete);
-
-
-//Desactive and active users
-router.patch('/api/usuarios/desactive/:id');
-router.patch('/api/usuarios/active/:id');
-
-// UPLOAD AND DOWNLOAD FILES
-<<<<<<< HEAD
-//router.post("/upload", auth, FileController.upload);
-router.post("/upload", auth, upload.single("file"), DocumentoController.create)
-router.get('/api/documentos/:id', auth, DocumentoController.find, DocumentoPolicy.show, DocumentoController.show);
-//router.get("/files",auth, FileController.getListFiles);
-// router.get("/files/:name",auth, FileController.download);
-=======
-router.post("/upload");
-router.get("/files");
-router.get("/files/:name");
->>>>>>> d1410144e3310d9b67fd23d9d6a86556bce65f54
-
-
-//Registro y login
-router.post('/api/signin');
-router.post('/api/signup');
-//router.post('/api/recuperar-contrasena', AuthController.changePassword);
-router.patch('/api/recuperar-contrasena')
->>>>>>> d293bf755d1ea5dcfe250d2cbf30c1736e90ef2e
-
-
-module.exports = router;
+module.exports = router
